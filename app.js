ccfaf9089d817d245dd8b8628dd04d9d25100b3f
mpicpus <mpicpus@gmail.com>
2020-01-16 11:03:55 +0100

    indentation final fix => added .editorconfig

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Organized dictionary.
-  // [baseName, teensName, tensPrefix]
-  let numbersDictionary = {
-    0: ['zero', 'ten'],
-    1: ['one', 'eleven'],
-    2: ['two', 'twelve', 'twen'],
-    3: ['three', 'thirteen', 'thir'],
-    4: ['four', null, 'for'],
-    5: ['five', 'fifteen', 'fif'],
-    6: ['six'],
-    7: ['seven'],
-    8: ['eight', 'eighteen', 'eigh'],
-    9: ['nine']
-  }
+	// Organized dictionary.
+	// [baseName, teensName, tensPrefix]
+	let numbersDictionary = {
+		0: ['zero', 'ten'],
+		1: ['one', 'eleven'],
+		2: ['two', 'twelve', 'twen'],
+		3: ['three', 'thirteen', 'thir'],
+		4: ['four', null, 'for'],
+		5: ['five', 'fifteen', 'fif'],
+		6: ['six'],
+		7: ['seven'],
+		8: ['eight', 'eighteen', 'eigh'],
+		9: ['nine']
+	}
 
 
-  // Main parsing toolbox magic object.
-  // Could also have been a class, but why bother...?
-  let parser = {
-    teenifyNext: false,
-    hundrifyNext: false,
-    thousifyNext: false,
-    overThousifyNext: false,
+	// Main parsing toolbox magic object.
+	// Could also have been a class, but why bother...?
+	let parser = {
+		teenifyNext: false,
+		hundrifyNext: false,
+		thousifyNext: false,
+		overThousifyNext: false,
 
-    // Units. Will also handle teens if present.
-    0: (n) => {
-      let response = '';
-      let and = this.hundrifyNext ? ' and ' : '';
-      
-      this.hundrifyNext = false;
+		// Units. Will also handle teens if present.
+		0: (n) => {
+			let response = '';
+			let and = this.hundrifyNext ? ' and ' : '';
+			
+			this.hundrifyNext = false;
 
-      if (this.teenifyNext) {
-        response = numbersDictionary[n][1] ?
+			if (this.teenifyNext) {
+				response = numbersDictionary[n][1] ?
        `${numbersDictionary[n][1]}` :
        ` ${numbersDictionary[n][0]}teen`;
 
@@ -44,108 +44,108 @@ document.addEventListener('DOMContentLoaded', () => {
     return ` ${and}${response}`;
   },
 
-    // Tens.
-    1: (n) => {
-      let response = '';
-      let and = this.hundrifyNext ? ' and ' : '';
+		// Tens.
+		1: (n) => {
+			let response = '';
+			let and = this.hundrifyNext ? ' and ' : '';
 
-      if (n == '1') {
-        this.teenifyNext = true;
-        this.hundrifyNext = false;
-      } else if (n != '0') {
-        response = ` ${numbersDictionary[n][2] ? numbersDictionary[n][2] : numbersDictionary[n][0]}ty`;
-        this.hundrifyNext = false;
-      } else
+			if (n == '1') {
+				this.teenifyNext = true;
+				this.hundrifyNext = false;
+			} else if (n != '0') {
+				response = ` ${numbersDictionary[n][2] ? numbersDictionary[n][2] : numbersDictionary[n][0]}ty`;
+				this.hundrifyNext = false;
+			} else
       and = '';
 
       return `${and}${response}`;
     },
 
-    // Hundreds.
-    2: (n) => {
-      response = '';
-      this.hundrifyNext = this.thousifyNext || n !='0';
+		// Hundreds.
+		2: (n) => {
+			response = '';
+			this.hundrifyNext = this.thousifyNext || n !='0';
+      
 
+			if (n != '0') {
+				response = `${this.thousifyNext ? ', ' : ' '}${numbersDictionary[n][0]} hundred`;
+			}
 
-      if (n != '0') {
-        response = `${this.thousifyNext ? ', ' : ' '}${numbersDictionary[n][0]} hundred`;
-      }
+			this.thousifyNext = false;
 
-      this.thousifyNext = false;
+			return `${response}`
+		},
 
-      return `${response}`
-    },
+		// Thousands.
+		3: (n) => {
+			let response = '';
+			let and = this.hundrifyNext ? ' and ' : '';
+			this.hundrifyNext = false;
 
-    // Thousands.
-    3: (n) => {
-      let response = '';
-      let and = this.hundrifyNext ? ' and ' : '';
-      this.hundrifyNext = false;
-
-      if (this.teenifyNext)
-        response = parser[0](n)
-      else if (n != '0') {
-        response = numbersDictionary[n][0];
-      } else
+			if (this.teenifyNext)
+				response = parser[0](n)
+			else if (n != '0') {
+				response = numbersDictionary[n][0];
+			} else
       and = '';
 
       this.thousifyNext = true;
       return ` ${and}${response} thousand`;
     },
 
-    // Tens of thousands.
-    4: (n) => {
-      return parser[1](n);
-    },
+		// Tens of thousands.
+		4: (n) => {
+			return parser[1](n);
+		},
 
-    // Hundreds of thousands.
-    5: (n) => {
-      return parser[2](n);
-    }
-  }
-
-
-  // Main DOM elements.
-  let formElement = document.querySelector('#input');
-  let responseElement = document.querySelector('.response');
-
-  formElement.focus();
+		// Hundreds of thousands.
+		5: (n) => {
+			return parser[2](n);
+		}
+	}
 
 
-  // Main event. Will fire on <enter>.
-  formElement.addEventListener('change', () => {
-    responseElement.innerHTML = formElement.value ? onFormChanged(formElement.value) : ' ';
-  })
+	// Main DOM elements.
+	let formElement = document.querySelector('#input');
+	let responseElement = document.querySelector('.response');
 
-  function onFormChanged(value) {
-    value = cleanInput(value);
-    formElement.value = value;
-
-    let split = value.split('');
-
-    let splitProcessed = split.map((n, index, arr) => {
-      return isNumber(n) && parser[arr.length - index - 1] ? parser[arr.length - index - 1](n) : '';
-    });
-
-    return cleanOutput(splitProcessed.join(''));
-  }
+	formElement.focus();
 
 
-  // Utils
-  function cleanInput(input) {
-    return input.replace(/[^0-9]|^0+/g, '');
-  }
+	// Main event. Will fire on <enter>.
+	formElement.addEventListener('change', () => {
+		responseElement.innerHTML = formElement.value ? onFormChanged(formElement.value) : ' ';
+	})
 
-  function cleanOutput(output) {
-    let response = capitalize(output.replace(/^ +| +$/g, ''));
-    return response == '' ? '"Zero"' : `"${response}"`;
-  }
+	function onFormChanged(value) {
+		value = cleanInput(value);
+		formElement.value = value;
 
-  function isNumber(input) {
-    return input.match(/[0-9]/) ? true : false
-  }
+		let split = value.split('');
 
-  function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)
-  }
+		let splitProcessed = split.map((n, index, arr) => {
+			return isNumber(n) && parser[arr.length - index - 1] ? parser[arr.length - index - 1](n) : '';
+		});
+
+		return cleanOutput(splitProcessed.join(''));
+	}
+
+
+	// Utils
+	function cleanInput(input) {
+		return input.replace(/[^0-9]|^0+/g, '');
+	}
+
+	function cleanOutput(output) {
+		let response = capitalize(output.replace(/^ +| +$/g, ''));
+		return response == '' ? '"Zero"' : `"${response}"`;
+	}
+
+	function isNumber(input) {
+		return input.match(/[0-9]/) ? true : false
+	}
+
+	function capitalize(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1)
+	}
 }, false);
